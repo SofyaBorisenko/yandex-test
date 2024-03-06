fb3b2f0979e391ccf116acadcd3435d5b51a57be
SofyaBorisenko <sofyaborisenko806@gmail.com>
2024-03-06 16:48:05 +0300

    finished mobile stages carousel and desktop participants carousel

--- js/script.js
+++ js/script.js
@@ -1,6 +1,169 @@
-const marqueeContent = document.querySelector('.marquee-content');
+document
+  .getElementById('left-hero-button')
+  .addEventListener('click', function () {
+    // координаты верхней границы секции
+    const lectionSection = document.getElementById('lection');
+    const lectionSectionTop = lectionSection.offsetTop;
 
-marqueeContent.addEventListener('animationiteration', () => {
-  const firstElement = marqueeContent.children[0];
-  marqueeContent.appendChild(firstElement);
-});
+    // скролл к секции
+    window.scrollTo({
+      top: lectionSectionTop,
+      behavior: 'smooth',
+    });
+  });
+
+document
+  .getElementById('right-hero-button')
+  .addEventListener('click', function () {
+    // координаты верхней границы секции
+    const participantsSection = document.getElementById('participants');
+    const participantsSectionTop = participantsSection.offsetTop;
+
+    // скролл к секции
+    window.scrollTo({
+      top: participantsSectionTop,
+      behavior: 'smooth',
+    });
+  });
+
+// **БЕГУЩАЯ СТРОКА**
+const ticker = document.querySelector('.ticker');
+const list = document.querySelector('.ticker__list');
+
+// **МОБИЛЬНАЯ КАРУСЕЛЬ**
+function initMobileCarousel() {
+  const mobileCarousel = document.querySelector("[data-target='carousel']");
+  const part = mobileCarousel.querySelector("[data-target='card']");
+  const mobRightButton = document.querySelector("[data-action='slideRight']");
+
+  const partCount = mobileCarousel.querySelectorAll(
+    "[data-target='card']"
+  ).length;
+
+  let currentIndex = 0; // Индекс текущей карточки
+
+  function nextSlide() {
+    // Переходим к следующей карточке
+    currentIndex++;
+    if (currentIndex >= partCount - 1) {
+      currentIndex = partCount - 1; // Ограничиваем индекс, чтобы не выйти за границы карусели
+      mobRightButton.disabled = true; // Делаем кнопку "вправо" неактивной на последней карточке
+    } else {
+      mobRightButton.disabled = false; // Делаем кнопку "вправо" активной, если не на последней карточке
+    }
+
+    // Скрываем или показываем кнопку "влево" в зависимости от индекса
+    document.querySelector("[data-action='slideLeft']").style.display =
+      currentIndex > 0 ? 'block' : 'none';
+
+    // Прокручиваем карусель до нужной карточки
+    mobileCarousel.style.transform = `translateX(-${currentIndex * 100}%)`;
+  }
+
+  // Устанавливаем интервал для автоматической смены карточек
+  const intervalId = setInterval(nextSlide, 4000);
+
+  // Обработчик события клика на кнопку "вправо"
+  mobRightButton.addEventListener('click', function () {
+    clearInterval(intervalId); // Останавливаем автоматическую смену карточек при клике
+    nextSlide();
+    intervalId = setInterval(nextSlide, 4000); // Запускаем автоматическую смену карточек снова
+  });
+}
+
+// **КАРУСЕЛЬ ДЛЯ ДЕСКТОПА
+function initDesktopCarousel() {
+  const carousel = document.querySelector("[data-target='carousel']");
+  const card = carousel.querySelector("[data-target='card']");
+  const leftButton = document.querySelector("[data-action='slideLeft']");
+  const rightButton = document.querySelector("[data-action='slideRight']");
+  const carouselWidth = carousel.offsetWidth;
+  const cardStyle = card.currentStyle || window.getComputedStyle(card);
+  const cardMarginRight = Number(cardStyle.marginRight.match(/\d+/g)[0]);
+  const cardCount = carousel.querySelectorAll("[data-target='card']").length;
+  let offset = 0;
+  const maxX = -(
+    (cardCount / 3) * carouselWidth +
+    cardMarginRight * (cardCount / 3) -
+    carouselWidth -
+    cardMarginRight
+  );
+  let autoScrollInterval; // интервал автоматического скролла
+  // запуск автоматического скролла
+  function startAutoScroll() {
+    autoScrollInterval = setInterval(() => {
+      slideRight();
+    }, 4000); // скролл каждые 4 сек.
+  }
+  // остановка автоматического скролла
+  function stopAutoScroll() {
+    clearInterval(autoScrollInterval);
+  }
+  // обработчики событий для кнопок "влево" и "вправо"
+  leftButton.addEventListener('click', function () {
+    stopAutoScroll();
+    slideLeft();
+  });
+  rightButton.addEventListener('click', function () {
+    stopAutoScroll();
+    slideRight();
+  });
+  const carouselPosition = document.querySelector('.carousel-position'); // элемент для отображения позиции карусели
+  const slideCount = 2; // общее количество слайдов (групп карточек)
+  // Функция для обновления позиции карусели
+  function updateCarouselPosition() {
+    const currentSlide = Math.ceil(
+      (Math.abs(offset) + carouselWidth) / (carouselWidth * slideCount)
+    ); // текущий слайд
+    carouselPosition.textContent = `${currentSlide * 3} / ${slideCount * 3}`; // обновление текста элемента
+  }
+  // Функция для обновления состояния кнопок
+  function updateButtonsState() {
+    // находится ли карусель в начальной позиции
+    if (offset === 0) {
+      leftButton.disabled = true; // если да, то отключаем кнопку "влево"
+    } else {
+      leftButton.disabled = false; // иначе включаем кнопку "влево"
+    }
+    // находится ли карусель в конечной позиции
+    if (offset === maxX) {
+      rightButton.disabled = true; // если да, то отключаем кнопку "вправо"
+    } else {
+      rightButton.disabled = false; // иначе включаем кнопку "вправо"
+    }
+  }
+  function slideLeft() {
+    stopAutoScroll(); // остановка автоскролла перед прокруткой
+    if (offset !== 0) {
+      offset += carouselWidth + cardMarginRight;
+      carousel.style.transform = `translateX(${offset}px)`;
+    } else {
+      offset = maxX;
+      carousel.style.transform = `translateX(${offset}px)`;
+    }
+    updateCarouselPosition(); // обновление позиции карусели
+    updateButtonsState(); // обновление состояния кнопок
+    startAutoScroll(); // возобновление автоскролла
+  }
+  function slideRight() {
+    stopAutoScroll(); // остановка автоскролла перед прокруткой
+    if (offset !== maxX) {
+      offset -= carouselWidth + cardMarginRight;
+      carousel.style.transform = `translateX(${offset}px)`;
+    } else {
+      offset = 0;
+      carousel.style.transform = `translateX(${offset}px)`;
+    }
+    updateCarouselPosition(); // обновление позиции карусели
+    updateButtonsState(); // обновление состояния кнопок
+    startAutoScroll(); // возобновление автоскролла после прокрутки
+  }
+  startAutoScroll();
+}
+
+// Запуск нужной карусели в зависимости от ширины экрана
+if (window.innerWidth <= 768) {
+  initMobileCarousel();
+} else {
+  initDesktopCarousel();
+}
---
+++ js/stages-mobile-carousel.js
@@ -0,0 +1,190 @@
+(function (root, factory) {
+  // Проверяем среду выполнения, чтобы понять, как экспортировать модуль
+  if (typeof define === 'function' && define.amd) {
+    define([], function () {
+      return factory();
+    });
+  } else if (typeof exports === 'object') {
+    module.exports = factory();
+  } else {
+    // Если нет AMD или CommonJS, просто экспортируем в глобальную область видимости
+    root.Carousel = factory();
+  }
+})(this, function () {
+  'use strict';
+
+  var Carousel = function (obj) {
+    // Конструктор класса Carousel
+    this.obj = document.querySelector(obj);
+    this.slide_index;
+    this.slide_length;
+    this.slide_current_obj;
+
+    this._init();
+  };
+
+  Carousel.prototype = {
+    constructor: Carousel,
+
+    _init: function () {
+      // Начальная настройка карусели
+      this.slide_index = this.obj.getAttribute('data-slide-index')
+        ? parseInt(this.obj.getAttribute('data-slide-index'))
+        : 0;
+      this._updateCurrentSlideObj();
+      this.slide_current_obj.className += ' active';
+      this.slide_length = this.obj.querySelectorAll('.carousel-item').length;
+      this.animationEnd = this.whichAnimationEvent();
+      this._setupHandlers();
+      this._updateButtonsState(); // обновление состояния кнопок
+      this._disableLeftButton(); // чтобы изначально кнопка влево была отключена
+    },
+
+    // Отключения кнопки влево
+    _disableLeftButton: function () {
+      var btn_L = this.obj.querySelector(".carousel-btn[data-dir='_L']");
+      btn_L.disabled = true;
+    },
+
+    _setupHandlers: function () {
+      // Установка обработчиков событий для кнопок и точек карусели
+      var self = this;
+
+      var btn_L = this.obj.querySelector(".carousel-btn[data-dir='_L']");
+      btn_L.addEventListener('mousedown', function () {
+        self._slideLeft();
+      });
+
+      var btn_R = this.obj.querySelector(".carousel-btn[data-dir='_R']");
+      btn_R.addEventListener('mousedown', function () {
+        self._slideRight();
+      });
+    },
+
+    _updateCurrentSlideDot: function () {
+      // Обновление активной точки
+      var dots = this.obj.querySelectorAll('.carousel-dot');
+      for (var i = 0; i < dots.length; i++) {
+        if (i == this.slide_index) {
+          dots[this.slide_index].className += ' active';
+        } else {
+          dots[i].className = dots[i].className.replace(/(^| )active/, '');
+        }
+      }
+    },
+
+    _updateCurrentSlideObj: function () {
+      // Обновление текущего слайда
+      this.slide_current_obj = this.obj.querySelector(
+        ".carousel-item[data-slide-index='" + this.slide_index + "']"
+      );
+      this._updateCurrentSlideDot();
+    },
+
+    _slide: function (dir) {
+      // Главная функция перемещения слайдов
+      var carousel = this.obj;
+      carousel.className += ' preventDoubleTap';
+
+      var class_for_current = dir == '_R' ? 'prev' : 'next';
+      var class_for_target = dir == '_R' ? 'next' : 'prev';
+
+      var current_slide = this.slide_current_obj;
+      current_slide.className += ' ' + class_for_current;
+      current_slide.className = current_slide.className.replace(
+        /(^| )active/,
+        ''
+      );
+      current_slide.addEventListener(this.animationEnd, function () {
+        current_slide.className = current_slide.className.replace(
+          new RegExp('(^| )' + class_for_current, 'g'),
+          ''
+        );
+        current_slide.removeEventListener(this.animationEnd);
+      });
+
+      var target_slide = this.obj.querySelector(
+        ".carousel-item[data-slide-index='" + this.slide_index + "']"
+      );
+      target_slide.className += ' ' + class_for_target;
+      target_slide.className += ' active';
+      current_slide.addEventListener(this.animationEnd, function () {
+        target_slide.className = target_slide.className.replace(
+          new RegExp('(^| )' + class_for_target, 'g'),
+          ''
+        );
+        carousel.className = carousel.className.replace(
+          new RegExp('(^| )' + 'preventDoubleTap', 'g'),
+          ''
+        );
+        target_slide.removeEventListener(this.animationEnd);
+      });
+
+      this._updateCurrentSlideObj();
+    },
+    // Слайд влево
+    _slideLeft: function () {
+      if (this.slide_index == 0) {
+        return; // Перемещение к слайдам останавливается, если текущий слайд первый (с индексом 0)
+      } else {
+        this.slide_index -= 1; // Если слайд не первый, перемещаемся к предыдущему
+        this._slide('_L');
+        this._updateButtonsState(); // Обновляем состояние кнопок после перемещения
+      }
+    },
+    // Слайд вправо
+    _slideRight: function () {
+      if (this.slide_index == this.slide_length - 1) {
+        return; // Перемещение к слайдам останавливается, если текущий слайд последний
+      } else {
+        this.slide_index += 1; // Если слайд не последний, перемещаемся к следующему
+        this._slide('_R');
+        this._updateButtonsState(); // Обновляем состояние кнопок после перемещения
+      }
+    },
+
+    _updateButtonsState: function () {
+      // Получаем ссылки на кнопки влево и вправо
+      var btn_L = this.obj.querySelector(".carousel-btn[data-dir='_L']");
+      var btn_R = this.obj.querySelector(".carousel-btn[data-dir='_R']");
+      // Устанавливаем состояние кнопки влево в зависимости от текущего слайда
+      if (this.slide_index === 0) {
+        btn_L.disabled = true;
+      } else {
+        btn_L.disabled = false;
+      }
+      // Устанавливаем состояние кнопки вправо в зависимости от текущего слайда
+      if (this.slide_index === this.slide_length - 1) {
+        btn_R.disabled = true;
+      } else {
+        btn_R.disabled = false;
+      }
+    },
+
+    whichAnimationEvent: function () {
+      // Определение события завершения анимации CSS
+      var t;
+      var el = document.createElement('fakeelement');
+      var animations = {
+        animation: 'animationend',
+        OAnimation: 'oAnimationEnd',
+        MozAnimation: 'animationend',
+        WebkitAnimation: 'webkitAnimationEnd',
+      };
+
+      for (t in animations) {
+        if (el.style[t] !== undefined) {
+          return animations[t];
+        }
+      }
+    },
+  };
+
+  // Экспорт класса Carousel
+  return Carousel;
+});
+
+function main() {
+  // Запуск карусели при загрузке документа
+  stagesMobileCarousel = new Carousel('.stages-mobile-carousel');
+}
